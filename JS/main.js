a9db20059fedd1c04014bb19e51328ba2f2c24d6
Emmascobar <emmanuelrescobar@gmail.com>
2022-12-15 17:24:51 +0100

    Inicio

---
+++ JS/main.js
@@ -0,0 +1,60 @@
+
+// Variables ejercicio DOS:
+
+const nombre = `Emmanuel`;
+let edad = 34;
+let mayorDeEdad = `Si`;
+const direccion = { direccion: `Carrer del Arte 84` };
+let coloresFavoritos = ["rojo", "negro", "blanco"];
+let lenguajesFavoritos = ["html", "css", "JavaScrip"];
+const mejorLenguaje = `html`;
+const peorLenguaje = `css`;
+
+// ejercicio TRES:
+let numero1 = 5;
+let numero2 = 4;
+
+let resultado = numero1 + numero2;
+console.log(resultado);
+
+// Otras operaciones.
+console.log(numero1 - numero2);
+console.log(numero1 * numero2);
+console.log(numero1 / numero2);
+
+// Ejercicio CUATRO
+let saludo = `hola`;
+
+if (saludo = `hola`) {
+    console.log(`Ha saludado`)
+} else {
+    console.log(`No ha saludado`);
+}
+
+// Ejercicio CINCO
+
+let pokemons = ["vaporeon", "umbreon", "flareon"];
+
+console.log(pokemons[0]);
+console.log(pokemons[1]);
+console.log(pokemons[2]);
+
+
+let pokemones = [
+    pokemon1 = {
+        nombre: "vaporeon",
+        tipo: `agua`,
+    },
+    pokemon2 = {
+        nombre: "Umbreon",
+        tipo: `Psiquico`,
+    },
+    pokemon1 = {
+        nombre: "Flareon",
+        tipo: `fuego`,
+    }
+]
+
+if (pokemones.tipo = `fuego`) {
+    console.log(`Es un pokemon tipo FUEGO`)
+}
